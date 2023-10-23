080b480001e2a0ef23e1a55df1c8332d37c21215
Èric Salat <ericsalat@protonmail.com>
2023-10-23 13:04:14 +0200

    Added a functionality to search repos by its owner

--- src/components/RepositoryLayout/RepositoryLayout.tsx
+++ src/components/RepositoryLayout/RepositoryLayout.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import RepositoryList from "../RepositoryList/RepositoryList";
+import SearchUser from "../SearchUser/SearchUser";
 import SearchBar from "../SearchBar/SearchBar";
 import GitHubDataContext from "../../context/GitHubDataContext";
 import { fetchDataFromGitHub } from "../../logic/GitHubAPICall";
 import DropdownButton from "../DropdownButton/DropwdownButton";
 
 function RepositoryLayout() {
-    const username = "EricSalat";
+    const [username, setUsername] = useState<string>("EricSalat");
     const [repositories, setRepositories] = useState<any[]>([]);
     const [searchTerm, setSearchTerm] = useState<string>("");
     const [selectedLanguage, setSelectedLanguage] = useState<string>("");
@@ -20,15 +21,25 @@ function RepositoryLayout() {
                 setDataIsLoading(true);
             })
             .catch((error) => console.error("Error fetching the data: ", error));
-    }, []);
+    }, [username]);
 
     const handleLanguageSelect = (language: string) => {
         setSelectedLanguage(language);
     };
 
+    useEffect(() => {
+        console.log("El username ha cambiado a: ", username);
+      }, [username]);
+
     return (
         <GitHubDataContext.Provider value={{ repositories }}>
             <div>
+                
+                <SearchUser 
+                    onSubmit={(newUsername) => {
+                        setUsername(newUsername); // Actualizar el estado username en RepositoryLayout
+                      }}
+                />
                 <SearchBar
                     searchTerm={searchTerm}
                     onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
@@ -47,6 +58,8 @@ function RepositoryLayout() {
             </div>
         </GitHubDataContext.Provider>
     );
+
+   
 }
 
 export default RepositoryLayout;
---
+++ src/components/SearchUser/SearchUser.tsx
@@ -0,0 +1,32 @@
+import "./SearchUser.css";
+import { useState } from 'react';
+
+interface SearchUserProps {
+  onSubmit: (newUsername: string) => void;
+}
+
+function SearchUser({ onSubmit }: SearchUserProps) {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        onSubmit(inputValue); // Enviar el valor introducido a la función onSubmit
+      };
+  
+  
+    return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search by username..."
+        value={inputValue}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setInputValue(event.target.value)
+        }
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default SearchUser;
